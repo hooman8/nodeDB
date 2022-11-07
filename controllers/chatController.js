fbfd6593fb14aef370139b59574a47a0e57e2c61
Hooman Pegahmehr <hpegahmehr@gmail.com>
2022-11-06 23:16:24 -0500

    unit 7 31.3

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,15 +1,29 @@
+const Message = require("../models/message");
 module.exports = (io) => {
   io.on("connection", (client) => {
     console.log("new connection");
+    Message.find({})
+      .sort({ createdAt: -1 })
+      .limit(10)
+      .then((messages) => {
+        client.emit("load all messages", messages.reverse());
+      });
     client.on("disconnect", () => {
       console.log("user disconnected");
     });
     client.on("message", (data) => {
       let messageAttributes = {
         content: data.content,
+        userName: data.userName,
         user: data.userId,
       };
-      io.emit("message", messageAttributes);
+      let aMessage = new Message(messageAttributes);
+      aMessage
+        .save()
+        .then(() => {
+          io.emit("message", messageAttributes);
+        })
+        .catch((error) => console.log(`error: ${error.message}`));
     });
   });
 };
